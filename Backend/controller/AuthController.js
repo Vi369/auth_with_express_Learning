43ce9dc852ae287cea064193fdc4c3a174b3fb11
Vikas singh varma <Vikashverma3001@gmail.com>
2023-12-13 17:58:44 +0530

    resolve

--- Backend/controller/AuthController.js
+++ Backend/controller/AuthController.js
@@ -1,18 +1,30 @@
 //required user model Schema
 const userModel = require("../model/auth.model");
 
-const signUp = (req, res)=>{
+const signUp = async(req, res)=>{
     const {name, email, password, confirmPassword} = req.body;
     console.log(name, email, password, confirmPassword);
     try {
       const userInfo = userModel(req.body);
+      const result = await userInfo.save();
+      return res.status(200).json({
+         success: true,
+         data: result
+      })
     } catch (error) {
-      
+      // if user data already has so throw duplicate error
+      if(error.code === 11000){
+         return res.status(400).json({
+            success: false,
+            message: "Account Already exists"
+         })
+      }
+      return res.status(400).json({
+         success: false,
+         message: e.message
+      })
     }
- return  res.status(200).json({
-    success: true,
-    message: "hnn successful"
- })  
+
 }
 
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 
 const app = require('./app');
 
