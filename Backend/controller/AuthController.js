44d5ce695be0637bdfa97bb78483336aaa540859
Vikas singh varma <Vikashverma3001@gmail.com>
2023-12-13 16:11:52 +0530

    working on login and registration func

--- Backend/controller/AuthController.js
+++ Backend/controller/AuthController.js
@@ -1,6 +1,14 @@
+//required user model Schema
+const userModel = require("../model/auth.model");
+
 const signUp = (req, res)=>{
     const {name, email, password, confirmPassword} = req.body;
     console.log(name, email, password, confirmPassword);
+    try {
+      const userInfo = userModel(req.body);
+    } catch (error) {
+      
+    }
  return  res.status(200).json({
     success: true,
     message: "hnn successful"
