1d7d55faf549fec05ea460d20ddb7a0a3bf1517d
Vikas singh varma <Vikashverma3001@gmail.com>
2023-12-14 18:59:14 +0530

    singup functionality complete

--- Backend/controller/AuthController.js
+++ Backend/controller/AuthController.js
@@ -1,10 +1,35 @@
 //required user model Schema
 const userModel = require("../model/auth.model");
+// email validator
+const emailValidator = require("email-validator");
 
 const signUp = async(req, res)=>{
     const {name, email, password, confirmPassword} = req.body;
     console.log(name, email, password, confirmPassword);
     try {
+      if(!name || !email || !password || !confirmPassword){
+         return res.status(400).json({
+            success: false,
+            message: "Every feild is required"
+         })
+      }
+      // validation check 
+      var validEmail = emailValidator.validate(email);
+      if(!validEmail){
+         console.log("please provide Valid Email");
+         return res.status(400).json({
+            success: false,
+            message: "please provide Valid Email"
+         })
+      }
+      if(password !== confirmPassword){
+         console.log("Password and confirm password don't match")
+         return res.status(400).json({
+            success: false,
+            message: "Password and confirm password don't match"
+         })
+      }
+
       const userInfo = userModel(req.body);
       const result = await userInfo.save();
       return res.status(200).json({
