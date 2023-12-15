82594212542b9daa7ac8c1edbd86160c0e1e3358
Vikas singh varma <Vikashverma3001@gmail.com>
2023-12-15 11:19:36 +0530

    resolve

--- Backend/routes/AuthRoute.js
+++ Backend/routes/AuthRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { signUp } = require('../controller/AuthController');
+const { signUp, signIn } = require('../controller/AuthController');
 
 const authRoutes = express.Router();
 
 authRoutes.post('/signup', signUp)
+authRoutes.post('/signin', signIn)
 
 
 module.exports = authRoutes;
